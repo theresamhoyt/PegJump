7696c7f604d401f3dae403a56595f7d31459a6f1
zahidabid <zahid.abid@maine.edu>
2016-04-12 09:59:00 -0400

    works on all individual test cases last one takes too long

--- PegJump/src/student/PegJump.java
+++ PegJump/src/student/PegJump.java
@@ -1,5 +1,4 @@
 package student;
-
 /* 
  * This Student class is meant to contain your algorithm.
  * You should implement the two static methods:
@@ -16,126 +15,155 @@ package student;
  *   a jump takes the peg 'from' over the peg in 'over' (removing it) and into 'dest'
  */
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Map;
-
 import pegJump.*;
 
 public class PegJump {
-	private static  int filledPegs = -1;
+
+	// initialize variables
 	private static boolean[] pegs = null;
-	private static Map<Integer,ArrayList<Jump>> jumpDictionary = new HashMap<Integer, ArrayList<Jump>>();
-	private static int currDictionaryLocation = 0;	
-	private static double jumpCnt = 0;
-	
+	private static int pegCount = 0;
+	private static double jumpCount = 0;
 
 	// returns a monte-carlo estimate of the number of promising nodes in the
 	// search tree.
 	public static double estimatePegJump(PegJumpPuzzle puzzle) {
-	
-		return 100;     // a guess
+		return 1000.0;     // a guess
 	}
-	
+
 	// simple example routine that just repeatedly finds the first valid
 	// jump until it fails
 	// this returns:
 	//   the number of jumps tried
 	//   and as a modifiable argument, it fills in the jumpList
 	public static double solvePegJump(PegJumpPuzzle puzzle, ArrayList<Jump> jumpList) 
-	{		
-		if(filledPegs == -1){
-			filledPegs = puzzle.numHoles() - 1;
-		}
-		if(pegs == null){
+	{	
+
+		// hashset to put all the seen boards in
+		HashSet<PegJumpPuzzle> seenBoards = new HashSet<PegJumpPuzzle>();
+
+		// initialize the peg array
+		if(pegs == null)
 			pegs = initalizePegArray(puzzle);
-		}
-		boolean foundAjump;
-		ArrayList<Jump> listOfJumps = new ArrayList<Jump>();
-    	Iterator<Jump> jitr = puzzle.jumpIterator();
-    	foundAjump = false;
-    	while(jitr.hasNext()){
-    		
-    		if(filledPegs == 1){
-    			foundAjump = false;
-    			break;
-    		}
-    		Jump j = jitr.next();
-			int from = j.getFrom();
-			int over = j.getOver();
-			int dest = j.getDest();
-			
-			int pegSize = pegs.length;
-			
-			if(from < pegSize && over < pegSize && dest < pegSize){
-				if(pegs[from] && pegs[over] && !pegs[dest]){
-					foundAjump = true;
-					listOfJumps.add(j);
-					
-					int dictonaryIndex = jumpDictionary.size();
-					jumpDictionary.put(dictonaryIndex, listOfJumps);
-					currDictionaryLocation++;
-				}
-				ArrayList<Jump> currList = jumpDictionary.get(currDictionaryLocation - 1);
-		    	if(foundAjump){
-		    		
-		    			Jump curr = currList.get(0);
-		    			makeJump(curr);
-		    			jumpList.add(curr);
-		    			solvePegJump(puzzle, jumpList);
-		    			foundAjump = false;
-			}else if(!jumpList.isEmpty()) {
-				//I thought this was where we should do the backtracking, but I cannot get it to work.
-				//jumpList = backtrack(jumpList);
-				
+
+		// iterate through all the nodes and adds valid jumps to the validJumps array
+		Iterator<Jump> jitr = puzzle.jumpIterator();
+		while(jitr.hasNext()){
+			Jump j = jitr.next();			
+
+			// if you find a valid solution
+			// do a jump and add it to the jump list
+			// recursively call solvePeg to get next jump
+			if(validJump(j)){
+				jumpList.add(makeJump(j));
+				solvePegJump(puzzle, jumpList);
 			}
+		}
+		// checks to see if the puzzle is solved
+		// this is when there is only one peg left
+		if(solved(pegCount, puzzle)){
+			return 0;
+		}
 
-    	}
-		
-    }
+		// puzzle not complete
+		// need to backtrack
+		backtrack(jumpList);
 
-    return jumpCnt;
-    
+		return 0;
+
 	}
+
 
 	private static ArrayList<Jump> backtrack(ArrayList<Jump> jumpList){
-		Jump j = jumpList.get(jumpList.size() - 1);
-		undoMove(j);
-		jumpList.remove(j);
-		
+
+		undoJump(jumpList.get(jumpList.size()-1));
+		jumpList.remove(jumpList.size()-1);
+
+		printJumpList(jumpList);
+		System.out.println();
 		return jumpList;
 	}
-	private static void undoMove (Jump jump) {
-		int from = jump.getFrom();
-		int over = jump.getOver();
-		int dest = jump.getDest();
-		pegs[from] = true;
-		pegs[over] = true;
-		pegs[dest] = false;
-		
-		
+
+	// print contents of the jumpList
+	static void printJumpList(ArrayList<Jump> jumpList){
+		for(Jump j: jumpList)
+			System.out.println(j);
 	}
-	private static Jump makeJump(Jump jump){
+
+	// checks to see if a jump is valid
+	// from and over have to be true
+	// dest have to be false
+	static boolean validJump(Jump jump){
+
 		int from = jump.getFrom();
 		int over = jump.getOver();
 		int dest = jump.getDest();
-	
+
+		if( pegs[from] && 
+				pegs[over] && 
+				!pegs[dest] )
+			return true;
+		else
+			return false;
+
+	}
+
+	// initialize the puzzle
+	// make an array to keep tracks of where the pegs are
+	// and put pegs in all holes except the starting hole
+	private static boolean[] initalizePegArray(PegJumpPuzzle puzzle){
+		pegs = new boolean[puzzle.numHoles()];  // hole numbers start at 0
+		pegCount = puzzle.numHoles()-1;
+		for (int i = 0; i < puzzle.numHoles(); i++)  
+			pegs[i] = true;                      // fill all holes
+		pegs[puzzle.getStartHole()] = false;     // clear starting hole
+
+		return pegs;
+	}
+
+	// checks to see if you have solved the puzzle
+	static boolean solved(int pegCount, PegJumpPuzzle puzzle){
+		return (pegCount==1 && pegs[puzzle.getStartHole()]);
+	}
+
+	// makes a jump by changing the boolean values
+	// or from, over, and dest
+	// increases jumpCount
+	static Jump makeJump(Jump jump){
+		int from = jump.getFrom();
+		int over = jump.getOver();
+		int dest = jump.getDest();
+
 		pegs[from] = false;
 		pegs[over] = false;
 		pegs[dest] = true;
-		jumpCnt++;
-		filledPegs--;
+
+		pegCount--;
+		jumpCount++;
+
+
 		return jump;
 	}
-	private static boolean[] initalizePegArray(PegJumpPuzzle puzzle){
-	    // initialize the puzzle
-		// make an array to keep tracks of where the pegs are
-		// and put pegs in all holes except the starting hole
-		pegs = new boolean[puzzle.numHoles()];  // hole numbers start at 0
-		for (int i = 0; i < puzzle.numHoles(); i++)  
-			pegs[i] = true;                      // fill all holes
-    	pegs[puzzle.getStartHole()] = false;     // clear starting hole
 
-    	return pegs;
+	// undoes the last move by changing the boolean values
+	// or from, over, and dest
+	// increases jumpCount 
+	static Jump undoJump (Jump jump) {
+		int from = jump.getFrom();
+		int over = jump.getOver();
+		int dest = jump.getDest();
+
+		pegs[from] = true;
+		pegs[over] = true;
+		pegs[dest] = false;
+
+		pegCount++;
+		jumpCount--;
+
+		return jump;
+
 	}
+
+
 }
