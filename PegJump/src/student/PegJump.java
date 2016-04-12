793bfa390dd64a6ce106bd686e28cf8d9605210f
Theresa Hoyt <theresammcgovern@gmail.com>
2016-04-11 21:58:20 -0400

    Worked on backtracking, it is still not working.

--- PegJump/src/student/PegJump.java
+++ PegJump/src/student/PegJump.java
@@ -1,5 +1,5 @@
 package student;
-import java.util.AbstractList;
+
 /* 
  * This Student class is meant to contain your algorithm.
  * You should implement the two static methods:
@@ -16,19 +16,25 @@ import java.util.AbstractList;
  *   a jump takes the peg 'from' over the peg in 'over' (removing it) and into 'dest'
  */
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+
 import pegJump.*;
 
 public class PegJump {
 	private static  int filledPegs = -1;
-	private static int jumpCnt = 0;
 	private static boolean[] pegs = null;
-
+	private static Map<Integer,ArrayList<Jump>> jumpDictionary = new HashMap<Integer, ArrayList<Jump>>();
+	private static int currDictionaryLocation = 0;	
+	private static double jumpCnt = 0;
 	
+
 	// returns a monte-carlo estimate of the number of promising nodes in the
 	// search tree.
 	public static double estimatePegJump(PegJumpPuzzle puzzle) {
-		return 1000.0;     // a guess
+	
+		return 100;     // a guess
 	}
 	
 	// simple example routine that just repeatedly finds the first valid
@@ -38,7 +44,6 @@ public class PegJump {
 	//   and as a modifiable argument, it fills in the jumpList
 	public static double solvePegJump(PegJumpPuzzle puzzle, ArrayList<Jump> jumpList) 
 	{		
-		
 		if(filledPegs == -1){
 			filledPegs = puzzle.numHoles() - 1;
 		}
@@ -46,66 +51,81 @@ public class PegJump {
 			pegs = initalizePegArray(puzzle);
 		}
 		boolean foundAjump;
-
+		ArrayList<Jump> listOfJumps = new ArrayList<Jump>();
     	Iterator<Jump> jitr = puzzle.jumpIterator();
-    	// start doing jumps
-    	do {
+    	foundAjump = false;
+    	while(jitr.hasNext()){
     		
     		if(filledPegs == 1){
+    			foundAjump = false;
     			break;
     		}
-			foundAjump = false;
-			while (jitr.hasNext()) {
-
-				Jump j = jitr.next();
-				int from = j.getFrom();
-				int over = j.getOver();
-				int dest = j.getDest();
-				
-				if (pegs[from] && pegs[over] && !pegs[dest]) {
-					// found a valid jump
-				      // add to the result list
-					
-					if( !jumpList.isEmpty() && jumpList.get(jumpList.size() - 1) == j){
-						jumpList.remove(j);
-						foundAjump = false;
-						filledPegs++;
-						break;
-					
-					}
-					jumpList.add(j); 
-					
-					pegs[from] = false;    // do the jump
-					pegs[over] = false;
-					pegs[dest] = true;
-					jumpCnt++;
-					foundAjump = true;
-					filledPegs = filledPegs - 1;
-		    		break;
-						
-				} 
+    		Jump j = jitr.next();
+			int from = j.getFrom();
+			int over = j.getOver();
+			int dest = j.getDest();
 			
+			int pegSize = pegs.length;
+			
+			if(from < pegSize && over < pegSize && dest < pegSize){
+				if(pegs[from] && pegs[over] && !pegs[dest]){
+					foundAjump = true;
+					listOfJumps.add(j);
+					
+					int dictonaryIndex = jumpDictionary.size();
+					jumpDictionary.put(dictonaryIndex, listOfJumps);
+					currDictionaryLocation++;
+				}
+				ArrayList<Jump> currList = jumpDictionary.get(currDictionaryLocation - 1);
+		    	if(foundAjump){
+		    		
+		    			Jump curr = currList.get(0);
+		    			makeJump(curr);
+		    			jumpList.add(curr);
+		    			solvePegJump(puzzle, jumpList);
+		    			foundAjump = false;
+			}else if(!jumpList.isEmpty()) {
+				//I thought this was where we should do the backtracking, but I cannot get it to work.
+				//jumpList = backtrack(jumpList);
+				
 			}
-		} while (foundAjump);
-    	
-    	if(filledPegs != 1){
-    		backtrack(jumpList);
-    		solvePegJump(puzzle, jumpList);
+
     	}
-    	filledPegs = -1;
-    	pegs = null;
-		return jumpCnt; 
+		
+    }
+
+    return jumpCnt;
+    
 	}
 
-	private static void backtrack (ArrayList<Jump> jumpList) {
-		Jump lastJump = jumpList.get(jumpList.size() - 1);
-		int from = lastJump.getFrom();
-		int over = lastJump.getOver();
-		int dest = lastJump.getDest();
+	private static ArrayList<Jump> backtrack(ArrayList<Jump> jumpList){
+		Jump j = jumpList.get(jumpList.size() - 1);
+		undoMove(j);
+		jumpList.remove(j);
+		
+		return jumpList;
+	}
+	private static void undoMove (Jump jump) {
+		int from = jump.getFrom();
+		int over = jump.getOver();
+		int dest = jump.getDest();
 		pegs[from] = true;
 		pegs[over] = true;
 		pegs[dest] = false;
 		
+		
+	}
+	private static Jump makeJump(Jump jump){
+		int from = jump.getFrom();
+		int over = jump.getOver();
+		int dest = jump.getDest();
+	
+		pegs[from] = false;
+		pegs[over] = false;
+		pegs[dest] = true;
+		jumpCnt++;
+		filledPegs--;
+		return jump;
 	}
 	private static boolean[] initalizePegArray(PegJumpPuzzle puzzle){
 	    // initialize the puzzle
